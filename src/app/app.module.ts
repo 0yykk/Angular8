7ec3ef7734dc6b2265e52d9723b3a75527bc5499
0yykk <921122271@qq.com>
2020-01-07 08:57:40 +0800

    angular list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {RouterModule}from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products.ts
@@ -0,0 +1,17 @@
+export const products=[
+    {
+        name: 'Phone',
+        price: 799,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        name: 'Ipad Pro',
+        price: 899,
+        description: 'A great Pad with one of the best cameras'
+    },
+    {
+        name: 'Ipad Air',
+        price: 699,
+        description: 'aaaaaaaaaaaaa'
+    }
+];
